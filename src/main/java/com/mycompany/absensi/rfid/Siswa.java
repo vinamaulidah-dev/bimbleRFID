421d070f4c793a6f86755e7c55b100dbc94507c1
Andra526 <andrasetya856@gmail.com>
2026-04-20 09:05:19 +0700

    new commit

---
+++ src/main/java/com/mycompany/absensi/rfid/Siswa.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.absensi.rfid;
+
+/**
+ *
+ * @author LOQ
+ */
+public class Siswa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
